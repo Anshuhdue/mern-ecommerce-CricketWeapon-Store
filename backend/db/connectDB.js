1c07fbfac3757b06fd684d8fa72e1f63a1547c06
Anshuhdue <123885795+Anshuhdue@users.noreply.github.com>
2025-06-30 18:14:05 +0530

    ki

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,16 +1,14 @@
-const mongoose = require("mongoose");
-require("dotenv").config({ path: "backend/config/config.env" })
-function connectDB(){
-    mongoose.set("strictQuery", false); 
-  
-    mongoose   
-        .connect(process.env.DB_LINK) 
-        .then(function () {
-            console.log("DB_connected");
-        })
-        .catch(function (err) {
-            console.log("error", err);
-        })
-}
+const mongoose = require('mongoose');
 
-module.exports = connectDB
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
